3d5a535a19c29601eaeb6f5253120b5f2c1db8c0
ReverieGameDev <officialzeralia@gmail.com>
2026-02-18 09:55:20 -0500

    2/18

---
+++ Assets/Prefabs/EnemyPrefab/MageSkeleton.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class MageSkeleton : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //heat seeking magic missle, attack to break the spells
+    //boulder drop
+    //veigar e
+    //fire ball that leaves behind a trail of fire
+}
---
+++ Assets/Prefabs/EnemyPrefab/WarriorSkeleton.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class WarriorSkeleton : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //charge attack
+    //in combination with the charge attack, the warrior then hits you with another ability like ambessa who charges in then uses an aoe attack then goes back to original position
+    //increase armor, shield 
+    //summoner summons and buffs AS
+}
