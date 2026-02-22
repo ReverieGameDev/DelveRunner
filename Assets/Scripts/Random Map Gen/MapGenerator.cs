d882608b99f6e8d91d3a4ff725b2769b2364cbd1
ReverieGameDev <officialzeralia@gmail.com>
2026-02-22 00:50:06 -0500

    room generation complete

--- Assets/Scripts/Random Map Gen/MapGenerator.cs
+++ Assets/Scripts/Random Map Gen/MapGenerator.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Room
@@ -5,18 +6,21 @@ public class Room
     public int centerX;
     public int centerY;
     public int radius;
-    public string roomType; // "fightNode", "cache", "hidden", "spawn", "boss"
+    public string roomType;
 }
+
 public class MapGenerator : MonoBehaviour
 {
-    public int mapHeight = 300;
-    public int mapWidth = 300;
-    private float perlinNoiseObstacle = 0.2f;
+    public int mapHeight = 500;
+    public int mapWidth = 500;
     public int[,] mapArray;
     private MapRenderer mapRenderer;
-    private float offsetX;
-    private float offsetY;
-    
+    public List<Room> rooms = new List<Room>();
+    public int leftBound = 20;
+    public int rightBound = 280;
+    public int bottomBound = 20;
+    public int topBound = 280;
+    private Room lastViableSpawn;
 
     void Start()
     {
@@ -36,10 +40,114 @@ public class MapGenerator : MonoBehaviour
             }
         }
 
-        CarveRoom(150, 150, 30);
-
+        PlaceRooms();
         mapRenderer.RenderMap();
     }
+
+    public void PlaceRooms()
+    {
+        // Spawn room - center of map
+        int spawnX = (leftBound + rightBound) / 2;
+        int spawnY = (bottomBound + topBound) / 2;
+        Room spawnRoom = new Room { centerX = spawnX, centerY = spawnY, radius = 15, roomType = "spawn" };
+        rooms.Add(spawnRoom);
+        CarveRoom(spawnX, spawnY, 15);
+
+        // Fight Nodes
+        for (int i = 0; i < Random.Range(2, 4); i++)
+        {
+            bool roomPlaced = false;
+
+            do
+            {
+                if (roomPlaced) { break; }
+                int viableSpawn = 0;
+                int roomX = Random.Range(leftBound, rightBound);
+                int roomY = Random.Range(bottomBound, topBound);
+                Vector2 newRoom = new Vector2(roomX, roomY);
+
+                for (int x = 0; x < rooms.Count; x++)
+                {
+                    Vector2 roomToCheckAgainst = new Vector2(rooms[x].centerX, rooms[x].centerY);
+                    if (Vector2.Distance(newRoom, roomToCheckAgainst) > 40)
+                    {
+                        Room currentRoom = new Room { centerX = roomX, centerY = roomY, radius = 20, roomType = "fightNode" };
+                        lastViableSpawn = currentRoom;
+                        viableSpawn++;
+                    }
+                }
+
+                if (viableSpawn == rooms.Count)
+                {
+                    rooms.Add(lastViableSpawn);
+                    CarveRoom(roomX, roomY, 20);
+                    roomPlaced = true;
+                }
+            }
+            while (!roomPlaced);
+        }
+
+        // Caches
+        for (int i = 0; i < Random.Range(1, 3); i++)
+        {
+            bool roomPlaced = false;
+
+            do
+            {
+                int viableSpawn = 0;
+                int roomX = Random.Range(leftBound, rightBound);
+                int roomY = Random.Range(bottomBound, topBound);
+                Vector2 newRoom = new Vector2(roomX, roomY);
+
+                for (int x = 0; x < rooms.Count; x++)
+                {
+                    Vector2 roomToCheckAgainst = new Vector2(rooms[x].centerX, rooms[x].centerY);
+                    if (Vector2.Distance(newRoom, roomToCheckAgainst) > 10)
+                    {
+                        viableSpawn++;
+                    }
+                }
+
+                if (viableSpawn == rooms.Count)
+                {
+                    Room currentRoom = new Room { centerX = roomX, centerY = roomY, radius = 5, roomType = "cache" };
+                    rooms.Add(currentRoom);
+                    CarveRoom(roomX, roomY, 5);
+                    roomPlaced = true;
+                }
+            }
+            while (!roomPlaced);
+        }
+
+        // Boss room
+        bool bossPlaced = false;
+        do
+        {
+            int viableSpawn = 0;
+            int roomX = Random.Range(leftBound, rightBound);
+            int roomY = Random.Range(bottomBound, topBound);
+            Vector2 newRoom = new Vector2(roomX, roomY);
+
+            for (int x = 0; x < rooms.Count; x++)
+            {
+                Vector2 roomToCheckAgainst = new Vector2(rooms[x].centerX, rooms[x].centerY);
+                if (Vector2.Distance(newRoom, roomToCheckAgainst) > 30)
+                {
+                    viableSpawn++;
+                }
+            }
+
+            if (viableSpawn == rooms.Count)
+            {
+                Room bossRoom = new Room { centerX = roomX, centerY = roomY, radius = 25, roomType = "boss" };
+                rooms.Add(bossRoom);
+                CarveRoom(roomX, roomY, 25);
+                bossPlaced = true;
+            }
+        }
+        while (!bossPlaced);
+    }
+
     public void CarveRoom(int centerX, int centerY, int radius)
     {
         float noiseOffset = Random.Range(0f, 10000f);
@@ -61,6 +169,7 @@ public class MapGenerator : MonoBehaviour
             }
         }
     }
+
     public void RemoveUnwantedTiles()
     {
         for (int i = 0; i < mapHeight; i++)
